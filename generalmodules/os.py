a7707910da75b6386cdaee1a81c1b408278c2e71
sda134 <60540655+sda134@users.noreply.github.com>
2021-07-30 17:01:25 +0900

    environ 追加

--- generalmodules/os.py
+++ generalmodules/os.py
@@ -7,6 +7,7 @@ import os
 # パス関連
 # path ：https://docs.python.org/ja/2.7/library/os.path.html
 print('__file__:\t%s' % __file__)
+print('environ:\t%s' % os.environ['HOME'])                  # 環境変数を使用 environ.get("home") でも良い
 print('abspath:\t%s' % os.path.abspath(__file__))           # abspath：絶対path を返す __file__ と何が違う？
 print('upperdir:\t%s' % os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 print('dirname:\t%s' % os.path.dirname(__file__))           # dirname：ファイル名を省いたディレクトリ名を返す
