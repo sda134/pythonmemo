e85c0eb5e0269337344ba74bb09c81e361b80d59
sda134 <60540655+sda134@users.noreply.github.com>
2022-03-01 14:15:31 +0900

    datetime, time 編集

--- generalmodules/datetime.py
+++ generalmodules/datetime.py
@@ -2,10 +2,12 @@
 #coding: UTF-8
 
 import datetime
+from dateutil.relativedelta import relativedelta
 
 # 基本
 print(datetime)             
 print("hour: %s, msec: %s" % (datetime.datetime.now().hour, datetime.datetime.now().microsecond))           
+print('datetime: %s' % repr( datetime.now()))
 
 # 文字列への変換
 print("%s" % (datetime.datetime.now().strftime('%Y/%m/%d %H:%M:%S')))
@@ -18,6 +20,7 @@ custom_dt = datetime.datetime(2022, 1, 23, hour=0, minute=0, second=0, microseco
 # 日時計算
 tomorrow = datetime.date.today() + datetime.timedelta(days=1)
 yesterday = datetime.date.today() - datetime.timedelta(days=1)
+last_month = datetime.date.today() - relativedelta(months=1)
 print("tomorrow: %s, yesterday: %s" % (tomorrow, yesterday))             
 
 
