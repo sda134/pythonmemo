c544c9c9f9fe1a48965c3082b1ac06b15d59add9
sda134 <60540655+sda134@users.noreply.github.com>
2022-03-16 13:56:30 +0900

    datetime.strptime の追加

--- generalmodules/datetime.py
+++ generalmodules/datetime.py
@@ -12,6 +12,8 @@ print('datetime: %s' % repr( datetime.now()))
 # 文字列への変換
 print("%s" % (datetime.datetime.now().strftime('%Y/%m/%d %H:%M:%S')))
 
+# 文字列からの変換
+dt_from_str = datetime.datetime.strptime('1999-12-31 00:00:00', '%Y/%m/%d %H:%M:%S')
 
 # イニシャライザ
 custom_dt = datetime.datetime(2022, 1, 23, hour=0, minute=0, second=0, microsecond=0, tzinfo=None)
