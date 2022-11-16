8266ac5d20e4d9344d26addf08283d3535b94dc5
sda134 <60540655+sda134@users.noreply.github.com>
2022-11-16 22:35:09 +0900

    1116_add_filter

--- generalmodules/filter.py
+++ generalmodules/filter.py
@@ -1,12 +1,21 @@
 
-  l = [-2, -1, 0, 1, 2]
+my_list = [-2, -1, 0, 1, 2]
 
-  # in
-  #for d in dict_test if d.
 
 # 基本
 # filter(関数object, リテラルobject)
+# イテレータを返す点に注意
+my_filter = filter(lambda x: x > 0, my_list):
 
 # filter
-for d in filter(lambda x: x > 0, l):
-    print(d)
+for ival in my_filter:
+    print(ival)
+
+# list に変換する方法
+list_from_filter = list(my_filter)
+
+
+
+# 補足：比較
+# map() は各要素に処理を行う
+# reduce()関数はすべての要素を一つの値に
--- list/dict.py
+++ list/dict.py
@@ -3,20 +3,23 @@
 
 # https://docs.python.org/ja/3/library/stdtypes.html?highlight=dict#dicts
 
-# dict 型の基本　※注目すべきは型の異なるメンバを含む事ができる点
+# flatな dict 型　※注目すべきは型の異なるメンバを含む事ができる点
 my_dict = {'key1': 10, 'key2': 'foo'}
+# dict のlist
+list_person = [
+    { 'name':'Taro', 'height':170, 'weight':60 },
+    { 'name':'Jiro', 'height':180, 'weight':80 },
+    { 'name':'Hanako', 'height':160, 'weight':50 }
+]
+
+
+# dict 型の基本　※注目すべきは型の異なるメンバを含む事ができる点
 my_dict['key3'] = 'o1'                      # キーを追加（通常は値を代入）
 print(my_dict['key3'])                      # キー　で値にアクセスする
 
 other_dict = {'key1': 4.0, 'key4': 4.0}     # 
 my_dict.update(other_dict)                  # update キーが存在する場合は更新、存在しない場合は追加
 print('\n' + '-' * 30)
-
-
-
-# 基本メソッド類
-key2 = my_dict.pop('key2')
-#del key2['key2'] でも同様。del key2['key1'] del key2['key2'] と複数要素を削除できる。
 
 
 print('key5 not in mydict:%s' % ('key5' not in my_dict))        # key in dict / key not in dict
@@ -46,6 +49,23 @@ print('values()')
 for dictVal in my_dict.values(): # 値
     print(dictVal)
 print('\n' + '-' * 30)
+
+
+
+
+# --- 基本メソッド類 ---
+# pop
+key2 = my_dict.pop('key2')
+#del key2['key2'] でも同様。del key2['key1'] del key2['key2'] と複数要素を削除できる。
+
+# get
+name_list = [ d.get('name') for d in list_person]
+print('name_list:%s' % name_list)
+
+# filter を使った検索
+taro_filter = filter(lambda item : item['name'] == 'Taro', list_person)
+taro_list = list(filter(lambda item : item['name'] == 'Taro', list_person))
+print('taro_list:%s' % taro_list)
 
 
 
